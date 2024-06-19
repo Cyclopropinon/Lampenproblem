1aea6efc9de12422090c84e21574787c3b65b248
Cyclopropinon <cyclopropinon@realprogrammers.org>
2024-06-19 13:43:50 +0200

    Make it compileable without FLINT Teil 4

--- Lampenproblem/compile.sh
+++ Lampenproblem/compile.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 chmod +x compile.sh
+
 #g++-13 -o Lampenproblem Lampenproblem.cpp -lgmp -lgmpxx -lncurses -O2 -march=native
 #g++-13 -o Lampenproblem Lampenproblem.cpp -lgmp -lgmpxx -lncursesw -DNCURSES_WIDECHAR=1 -O2 -march=native
 #g++-13 -o Lampenproblem Lampenproblem.cpp -lgmp -lgmpxx -lflint -lflintxx -lncursesw -DNCURSES_WIDECHAR=1 -O2 -march=native
 g++-13 -o Lampenproblem Lampenproblem.cpp -lgmp -lgmpxx -lflint -lncursesw -DNCURSES_WIDECHAR=1 -O2 -march=native
-cp -v Lampenproblem ./IDA/
+
+#cp -v Lampenproblem ./IDA/
 strip Lampenproblem
 cp -v Lampenproblem ./testumgebung/
--- Lampenproblem/makrocheks.h
+++ Lampenproblem/makrocheks.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #ifndef _ENABLEBIGINTS_
+    #line 17 "Lampenproblem.cpp"
     #warning "Is [Boost](https://www.boost.org/) installed? If yes, is it correctly modified to support bigints? Compiling without bigints..."
 #endif
 
 #ifdef _DISABLELIBFLINT_
+    #line 35 "Lampenproblem.cpp"
     #warning "[FLINT](https://flintlib.org/) not found! Compiling without FLINT..."
 #endif
 
 #ifdef _DISABLELIBFLINTXX_
+    #line 35 "Lampenproblem.cpp"
     #warning "[FLINTxx](https://github.com/flintlib/flintxx) not found! Compiling without FLINTxx..."
 #endif
