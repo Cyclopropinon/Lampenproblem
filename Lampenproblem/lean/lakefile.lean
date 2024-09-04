eb2dee74213554783f51970bf0f0c8d777606aec
Cyclopropinon <cyclopropinol@gmail.com>
2024-09-04 09:27:50 +0200

    Add Lean Teil 1

---
+++ Lampenproblem/lean/lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "Lampenproblem" where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «Lampenproblem» where
+  -- add any library configuration options here
