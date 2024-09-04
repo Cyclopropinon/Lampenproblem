36bad5d3f2a2e4c7e9a81dddd9bd7e5b075c463c
Cyclopropinon <cyclopropinol@gmail.com>
2024-09-04 17:10:43 +0200

    Add Lean Teil 6

--- Lampenproblem/lean/Lampenproblem/Formalisierung.lean
+++ Lampenproblem/lean/Lampenproblem/Formalisierung.lean
@@ -20,19 +20,31 @@ def flip_at {v: BitVec n} (m: Nat): (BitVec n) := BitVec.xor v (flip_at_helper m
 --def flip_at: BitVec n → m → BitVec n := sorry
 inductive LampState: BitVec n  → (m: Nat) → (k: Nat) → Type where
   | start: LampState all_off 1 1
-  | step:  LampState v m k → LampState (flip_at v m) (m + k) (m / n)
+  | step:  LampState v m k → LampState (flip_at m) (m + k) (m / n)
+  deriving Repr
+
+def PrintLampState (ls: @LampState n v m k): String :=
+  "n:          " ++ ToString.toString n ++
+  "\nDaten:    " ++ ToString.toString v ++
+  "\nSchritte: " ++ ToString.toString m ++
+  "\nRunden:   " ++ ToString.toString k ++ "\n"
 
 def step_n {n: Nat} {v: BitVec n} (NumSteps: Nat) (ls: LampState v m k) : Σ (v': BitVec n) (m' k' : Nat), LampState v' m' k' :=
   match NumSteps with
   | 0 => ⟨_, _, _, ls⟩
   | Nat.succ x => match step_n x ls with
-    | ⟨_, m', _, ls'⟩ => ⟨_, _, m'/n, ls'.step⟩
-    --| ⟨v', m', k', ls'⟩ => ⟨flip_at v' m', m' + k', m'/n, ls'.step⟩
+    --| ⟨_, m', _, ls'⟩ => ⟨_, _, m'/n, ls'.step⟩
+    | ⟨v', m', k', ls'⟩ => ⟨@flip_at n v' m', m' + k', m'/n, ls'.step⟩
 
 #print LampState.start
 #check BitVec
 #check BitVec.zero 3
 #check BitVec.allOnes 3
+
+#check ToString
+
+#eval IO.println (PrintLampState (@LampState.start 7))
+#eval IO.println (PrintLampState (step_n 5 (@LampState.start 7)))
 
 --#print BitVec.zero 3
 --#print BitVec.allOnes 3
