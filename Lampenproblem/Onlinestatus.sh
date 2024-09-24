8b75a7a823e442b98268e781741cded407bdb938
Cyclopropinon <cyclopropinon@realprogrammers.org>
2024-09-24 19:36:36 +0200

    Zusammenfassung streamen Teil 8

--- Lampenproblem/Onlinestatus.sh
+++ Lampenproblem/Onlinestatus.sh
@@ -18,6 +18,20 @@ fi
 # Port
 PORT=1729
 
+# Ermittele die lokale IP-Adresse des Systems (nur für eth0 oder wlan0, je nach Interface)
+LOCAL_IP=$(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n 1)
+
+# Wenn keine IP-Adresse gefunden wird, auf localhost zurückgreifen
+if [ -z "$LOCAL_IP" ]; then
+  LOCAL_IP="localhost"
+fi
+
+# Zeige, wie der Server von einem anderen Gerät aus aufgerufen werden kann
+echo "Die Datei kann von einem anderen Gerät über folgende URL aufgerufen werden:"
+echo "http://$LOCAL_IP:$PORT"
+echo "Bzw über folgenden Kommando:"
+echo "links2 http://$LOCAL_IP:$PORT"
+
 # Lausche auf Port 1729 und antworte mit dem Inhalt der Textdatei
 while true; do
   echo -e "Listening on port $PORT. Press [CTRL+C] to stop."
