c4cdec05aeadbc3936c87ae746135aaabc4ca76f
Cyclopropinon <cyclopropinon@realprogrammers.org>
2024-09-24 17:45:13 +0200

    Zusammenfassung streamen Teil 4

--- Lampenproblem/zusammenfassung.hh
+++ Lampenproblem/zusammenfassung.hh
@@ -5,21 +5,26 @@
 
 namespace fs = std::filesystem;
 
-void concatenateFiles(const std::string& directoryPath, const std::string& outputFilePath) {
+void concatenateFiles(const std::string& directoryPath, const std::string& outputFilePath)
+{
     std::ofstream outputFile(outputFilePath, std::ios::out | std::ios::app);
 
-    if (!outputFile.is_open()) {
+    if (!outputFile.is_open())
+    {
         std::cerr << "Konnte die Datei " << outputFilePath << " nicht öffnen.\n";
         return;
     }
 
     // Durchlaufe alle Unterverzeichnisse von "./abcd/"
-    for (const auto& dirEntry : fs::directory_iterator(directoryPath)) {
-        if (fs::is_directory(dirEntry)) {
+    for (const auto& dirEntry : fs::directory_iterator(directoryPath))
+    {
+        if (fs::is_directory(dirEntry))
+        {
             std::string summaryFilePath = dirEntry.path().string() + "/Zusammenfassung";
             std::ifstream inputFile(summaryFilePath);
 
-            if (inputFile.is_open()) {
+            if (inputFile.is_open())
+            {
                 std::cout << "Hänge Datei an: " << summaryFilePath << std::endl;
 
                 // Inhalte in die Ausgabe-Datei schreiben
@@ -36,7 +41,8 @@ void concatenateFiles(const std::string& directoryPath, const std::string& outpu
     std::cout << "Zusammengeführte Dateien in " << outputFilePath << " geschrieben.\n";
 }
 
-int main() {
+int main()
+{
     std::string directoryPath = "./abcd";
     std::string outputFilePath = "./Zusammenfassung.txt";
 
